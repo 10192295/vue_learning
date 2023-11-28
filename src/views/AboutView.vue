afed968f02aa1ff546a51b59e8f4b071d2ceef30
10192295 <10192295li_kai@cn.tre-inc.com>
2023-11-28 16:59:55 +0800

    init project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="about">
+    <button
+      @click="userInfo_2.count = 2"
+    >
+      {{ userInfo_2.count }}
+    </button>
+    <br />
+    <button
+      @click="userInfo.count = 3"
+    >{{ countComputed }}</button>
+  </div>
+</template>
+
+<!-- <template>
+  <div>
+    {{ countComputed }}
+  </div>
+</template> -->
+
+<script setup>
+import { computed, onMounted, ref } from "vue";
+
+  const count = ref(2)
+  // ref解包
+  const userInfo = ref({
+    count
+  })
+  const userInfo_2 = {
+    count: ref(1)
+  }
+
+  const countComputed = computed(() => {
+    return userInfo.value.count == 0 ? '改了' : '没改'
+  })
+  
+  onMounted(()  => {
+    console.log(count.value)
+    console.log(userInfo.value.count === count.value);
+    const { count } = userInfo_2
+  })
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    /* min-height: 100vh;
+    display: flex;
+    align-items: center; */
+  }
+}
+</style>
